8c2f70bc1f57cc903b353d5740f0421240b4e46b
ASSI-45 <zencolorbrush@gmail.com>
2024-01-10 16:10:19 +0100

    have a bare bones game loop, added a lot of comments

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from math import remainder
-
-# a class for the tik tak map 
+"""
+a class for the tik tak map storing all the game functions 
+"""
 class tik_tak_map:
     def __init__(self, w, h): 
-        self.w = w
-        self.h = h
+        self.w = int(w)
+        self.h = int(h)
         self.map = []
         self.rounds = 1
         self.player_turn = 1    
@@ -17,26 +17,29 @@ class tik_tak_map:
         # print(f"the with is {range(self.w)} and the hight {range(self.h)}")
         #---- 
         # player_turn = (self.rounds + 1) - self.rounds # bug in this line of code 
-        #---- 
+        #---- changes the player turn 
         match self.player_turn:
             case 1:
                 self.player_turn = 2
             case 2:
                 self.player_turn = 1
 
-        print(f"round {self.rounds} player {self.player_turn}") 
-        for x in range(self.h): 
+        print(f"round {self.rounds} player {self.player_turn}") # displays the round and player turn 
+        for x in range(self.h):
+            print(x)
             print(self.map[x][:]) # print whole array using a slice
 
-    def insert(self,input_hight,input_with,player):
+    def insert(self,input_hight,input_with):
         self.rounds = self.rounds + 1
-        match player:
+        # print(f"---- hight {input_hight} and with {input_with}")
+        # print(self.map) 
+        match self.player_turn: # draws the players moves
             case 1:
-                self.map[input_hight][input_with] = '|O|'
+                self.map[input_hight][input_with] = "|O|"
             case 2:
-                self.map[input_hight][input_with] = '|X|'
+                self.map[input_hight][input_with] = "|X|"
 
-    def test(self):
+    def test(self): # a test function you can test it. :)
         for x in range(self.h):
             self.map[x][0] = str(x) # self.map[h][w]
     
@@ -49,20 +52,35 @@ def intro():
     print("  a simple tik tow game  ")
     print("-------------------------")    
 
+"""
+the Game match system
+"""
+class Match(): 
+    def __init__(self):
+        self.game = tik_tak_map(3, 3)
+    
+    def create_match(self):
+        self.game.creation() 
+        self.game.display()
+    
+    def match_round(self, match_user_input):
+        Input = match_user_input.split()
+        # print(f'height{int(Input[0])}, with {int(Input[1])}') 
+        self.game.insert(int(Input[0]), int(Input[1])) # 0 = y , 1 = x on the playing field
+        self.game.display()
+
+"""
+the Game loop in the main function
+"""
 def main():
     intro()
-    user_input = input()
-    if user_input == "quit":    exit() 
+    game_match = Match() # creates an class instance of Match containing the match system
+    game_match.create_match() # creates a singel match 
+    while(True): # this will make the match last for iternity
+        print("Put in the x cordinate and then the y, with a space inbetween or not.") 
+        user_input = input()
+        if user_input == "quit":    exit() # exits the game of player types exit 
+        game_match.match_round(user_input) # a singel game match
 
 if __name__ == "__main__":
     main()
-
-m1 = tik_tak_map(3,3)
-m1.creation()
-m1.display()
-m1.insert(1,1,2)
-m1.display()
-m1.insert(1,0,1)  
-m1.display() 
-m1.insert(2,0,2)
-m1.display()
